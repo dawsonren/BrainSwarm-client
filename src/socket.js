a63bd3dfb4064fe115ef2a355ea5368050e1a847
alfenn <52635707+alfenn@users.noreply.github.com>
2023-04-16 12:12:33 -0500

    only read from server

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-// process.env.NODE_ENV === 'production'
-const URL = true ? 'https://squid-app-cojgk.ondigitalocean.app:4000' : 'http://localhost:4000';
+
+const URL = 'https://squid-app-cojgk.ondigitalocean.app:4000';
 
 export const socket = io(URL);
