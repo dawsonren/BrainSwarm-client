737e89184f9c95fc816f8c75822ba3937be826c9
alfenn <52635707+alfenn@users.noreply.github.com>
2023-04-16 12:45:10 -0500

    remove 4000 from client

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,6 @@ import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
 
-const URL = 'https://squid-app-cojgk.ondigitalocean.app:4000';
+const URL = 'https://squid-app-cojgk.ondigitalocean.app';
 
 export const socket = io(URL);
